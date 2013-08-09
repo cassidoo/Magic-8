ba3d6f20351479abeabe43b3615315fd2f2e3e15
Cassidy Williams <cassidoo@outlook.com>
2013-08-09 15:59:56 -0700

    First commit, images and app delegate stuff

---
+++ main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CrystalBall
+//
+//  Created by Williams, Cassidy on 7/10/13.
+//  Copyright (c) 2013 Cassidy Williams. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
